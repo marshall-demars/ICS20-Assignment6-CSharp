517f703dfb481f261b84b4ae6b642618ea92a133
Marshall Demars <99354910+marshall-demars@users.noreply.github.com>
2022-05-30 17:02:45 +0000

    main

--- main.cs
+++ main.cs
@@ -1,7 +1,31 @@
 using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static async Task Main()
+    {
+
+        HttpClient client = new HttpClient();
+        string response = await client.GetStringAsync(
+            "https://api.gameofthronesquotes.xyz/v1/random"
+        );
+        // Console.WriteLine(response);
+        var jsonAsDictionary = System.Text.Json.JsonSerializer.Deserialize<Object>(response);
+        // Console.WriteLine(jsonAsDictionary);
+        Console.WriteLine("");
+        JsonNode resultsNode = JsonNode.Parse(response)!;
+        // Console.WriteLine(characterNode);
+        JsonNode sentenceNode = resultsNode!["sentence"]!;
+        JsonNode characterNode = resultsNode!["character"]!;
+        JsonNode houseNode = resultsNode!["house"]!;
+        Console.WriteLine("");
+        Console.WriteLine(characterNode + ": " + sentenceNode + " " + houseNode);
+        Console.WriteLine("\nDone.");
+    }
 }
